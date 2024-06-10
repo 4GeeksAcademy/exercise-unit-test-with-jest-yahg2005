fe44d8b6bba293fb6047139b7c6465a5df97e126
Yahg2005 <161893815+Yahg2005@users.noreply.github.com>
2024-06-10 19:23:57 +0000

    Corregido lo de las variables y la logica

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 // test.js
-
+// one euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
 const { sum, fromEuroToDollar, fromDollarToYen,fromYenToPound } = require('./app.js');
 
 // Comienza tu primera prueba
